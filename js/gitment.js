019a0c5cee746ed2efd0f67287c4288c4c57ebb0
cs <cs876321722@outlook.com>
2022-08-29 22:26:03 +0800

    Site updated: 2022-08-29 22:26:03

--- js/gitment.js
+++ js/gitment.js
@@ -3412,12 +3412,12 @@ var Gitment = function () {
         link: replacedUrl
       }, options);
 
-      this.state.user.isLoggingIn = true;//https://blog.csdn.net/weixin_34008805/article/details/88585514
-      _utils.http.post('https://github.com/login/oauth/access_token', {//域名访问改自己域名 https://YourAppName.xx.com/
+      this.state.user.isLoggingIn = true;//域名https://cors.wenjunjiang.win/?https://github.com/login/oauth/access_token
+      _utils.http.post('https://gh-oauth.wowtalon.top', {//域名访问改自己域名 https://YourAppName.xx.com/
         code: code,
         client_id: client_id,
         client_secret: client_secret
-      }, '').then(function (data) {
+      }, '',{Accept:'application/json'}).then(function (data) {
         _this.accessToken = data.access_token;
         _this.update();
       }).catch(function (e) {
